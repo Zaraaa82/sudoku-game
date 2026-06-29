648a2fcf67f1a9275aa8fdae90019e0b08b1ef23
Zahraa Alaiwi <zozoa.jalil@gmail.com>
2026-06-29 11:31:41 +0300

    feat: add pause and timer functionality with updated popup and game status display

--- js/app.js
+++ js/app.js
@@ -1,13 +1,25 @@
 /*------------------------ Cached Element References ------------------------*/
+// Board Elements
 const boardEl = document.querySelector("#board");
-const rowEls = document.querySelectorAll(".row");
+// const rowEls = document.querySelectorAll(".row");
 const cellEls = [...document.querySelectorAll('.cell')];
 
+// Input Panel Elements 
 const inputPanelEl = document.querySelector('#input-panel');
 const numberBtnEls = [...document.querySelectorAll(".number-btn")];
-const heartEls = [...document.querySelectorAll('.heart')];
+
+// Mistake Chances Elements
+const heartEls = [...document.querySelectorAll(' .heart.chance')];
+
+// Timer Elements
+const [timeEl, timerBtnEl] = [...document.querySelectorAll('#timer-control *')]
+
+// Popup Elements
 const popupbackdropEl = document.querySelector('#popup-backdrop');
-const [popupHeaderEl,popupMessageEl, popupBtnEl] = [...document.querySelectorAll('#popup *')];
+const [popupHeaderEl,popupstatusEl,popupMessageEl, popupPrimaryBtnEl, popupSecondaryBtnEl] = [...document.querySelectorAll('#popup .pop-element')];
+const popupDifficultyEl = document.querySelector('#popup-difficulty');
+const popupheartsEls = [...document.querySelectorAll('#popup-mistakes .heart')];
+const popupTimeEl = document.querySelector('#popup-time');
 
 /*-------------------------------- Constants --------------------------------*/
 const boardCells = []; // Store cell Elements in 2D
@@ -40,19 +52,25 @@ const solution = [];
 const puzzle = [];
 
 /*---------------------------- Variables (state) ----------------------------*/
+// Selected Cell variables
 let selectedCell = null;
 let selectedRow = -1;
 let selectedCol = -1;
 let cellValue = null;
-
 let rowCells = [];
 let colCells = [];
 let boxCells = [];
 let relatedCells = [];
 let selectedSameNumber = [];
 
+// Game status Variables
+let currentDifficulty = 'Easy'; // will be changed later
 let mistakesCounter = 0;
-
+let timer = null;
+let time = {
+    seconds: 0,
+    minutes: 0
+}
 
 /*-------------------------------- Functions --------------------------------*/
 
@@ -66,22 +84,24 @@ function initializeGame(){
     addBoardBorders();
     loadPuzzle();
     selectStartingCell();
+    startTimer();
 }
 /*------------------------------- Game Setup -------------------------------*/
 
 function fetchCells() {
-  for (let row = 0; row < 9; row++) {
-    boardCells.push(cellEls.slice(row * 9, row * 9 + 9));
-  }
+    boardCells.length = 0;
+    for (let row = 0; row < 9; row++) {
+        boardCells.push(cellEls.slice(row * 9, row * 9 + 9));
+    }
 }
 
 function  addBoardBorders(){
     boardCells.forEach((row, rowIndex) => {
     row.forEach((cell, colIndex) => {
         if (colIndex === 2 || colIndex === 5 || colIndex === 8) {
-            updateClassList(cell, 'add', 'right-border');
+            updateClassList(cell,'add', 'right-border');
         } else if (colIndex === 0 || colIndex === 3 || colIndex === 6) {
-            updateClassList(cell, 'add', 'left-border');
+            updateClassList(cell,'add', 'left-border');
         }
 
         if (rowIndex === 2 || rowIndex === 5 || rowIndex === 8) {
@@ -108,6 +128,72 @@ function selectStartingCell(){
     let firstSelect = cellEls.find(cell=> !cell.classList.contains('fixed'));
     setSelectedCell(firstSelect);
 }
+/*-------------------------- Game status Management ---------------------------*/
+// Difficulty level Management
+
+// Mistake Management
+function handleMistake(){
+    mistakesCounter++;
+    let heart = heartEls[heartEls.length - mistakesCounter];
+    heart.src = './assets/images/Lost-chance.png';
+    heart.alt = 'Remaining chance';
+    if(mistakesCounter === heartEls.length){
+        stopTimer();
+        displayPopup('Game Over');
+    }
+}
+
+// Timer Management
+function startTimer(){
+    if (timer) 
+        return;
+    timer = setInterval(()=>{
+        time.seconds++;
+        if(time.seconds === 60){
+            time.seconds = 0;
+            time.minutes++;
+        }
+        if(time.minutes === 60){
+            displayPopup('Exceeded Time');
+            clearInterval(timer);
+            
+        }
+        timeEl.textContent = formatTimer();
+    },1000);
+}
+
+function pauseTimer(){
+    stopTimer();
+    timerBtnEl.dataset.status = 'paused';
+    displayPopup('Pause Timer');
+}
+function stopTimer(){
+    clearInterval(timer);
+    timer = null
+    timerBtnEl.src = './assets/images/play.png';
+    timerBtnEl.alt = 'Play Button';
+}
+function resumeTimer(){
+    timerBtnEl.src = './assets/images/pause.png';
+    timerBtnEl.alt = 'Pause Button'; 
+    timerBtnEl.dataset.status = 'playing'
+    startTimer();
+}
+function formatTimer(){
+    let seconds = (time.seconds < 10)? '0'+ time.seconds : time.seconds;
+    let minutes = (time.minutes < 10)? '0'+ time.minutes : time.minutes; 
+    return minutes + ':' + seconds;
+}
+function clearTimer(){
+    clearInterval(timer);
+    time.seconds = 0;
+    time.minutes = 0;
+    timer = null;
+    timerBtnEl.dataset.status = 'playing';
+    timerBtnEl.src = './assets/images/pause.png';
+    timeEl.textContent = '00:00';
+}
+
 /*-------------------------- Selection Management ---------------------------*/
 
 function handleCellClick(clickedCell){
@@ -162,7 +248,6 @@ function updateConflictingCellsHighlight(action){
         relatedCells.forEach(cell => updateClassList(cell, 'remove', 'conflicting-number'));
         return;
     }
-    // Always clear old conflicts
 
     if(!cellValue)
         return;
@@ -202,6 +287,11 @@ function findSameNumber(number){
 }
 
 /*--------------------------- Game Actions ---------------------------------*/
+function startNewGame(){
+    clearBoard();
+    initializeGame();
+}
+
 function handleNumberSelection(number){
 
     if(selectedCell.classList.contains('fixed')){
@@ -217,14 +307,15 @@ function handleNumberSelection(number){
             updateClassList(selectedCell, 'add', 'wrongNumber');
             updateClassList(selectedCell, 'remove', 'filled');
             handleMistake();
-
         } 
         else {
+
             updateClassList(selectedCell, 'remove', 'wrongNumber');
             updateClassList(selectedCell, 'add', 'filled');
         }
         setSelectedCell(selectedCell);
     }
+    console.log(detectWin());
     
 }
 
@@ -238,39 +329,6 @@ function eraseNumber(){
     }
 }
 
-function handleMistake(){
-    mistakesCounter++;
-    let heart = heartEls[heartEls.length - mistakesCounter];
-    heart.src = './assets/images/Lost-chance.png';
-    heart.alt = 'Remaining chance';
-    if(mistakesCounter === heartEls.length){
-        displayPopup('Game Over')
-    }
-}
-function displayPopup(displayType){
-    if(displayType == 'Game Over'){
-        popupHeaderEl.textContent = 'Game Over';
-        popupMessageEl.innerHTML = 'You\'ve reached the maximum number of mistakes.<br><br>Try again with a new puzzle!'
-        popupBtnEl.textContent = 'New Game';
-        updateClassList(popupBtnEl, 'add', 'mistakes');
-        updateClassList(popupbackdropEl, 'remove', 'hidden');
-    }
-}
-// managePopu(true, 'mistakes');
-function handlePopupBtnClick(){
-    if(popupBtnEl.classList.contains('mistakes')){
-        setTimeout(()=>{
-            updateClassList(popupBtnEl, 'remove', 'mistakes');
-            updateClassList(popupbackdropEl, 'add', 'hidden');
-            startNewGame();
-        },100)
-    }
-}
-function startNewGame(){
-    clearBoard();
-    clearSelected();
-    initializeGame();
-}
 function clearBoard(){
     cellEls.forEach(cell=>{
         cell.textContent = '';
@@ -282,6 +340,7 @@ function clearBoard(){
     });
     mistakesCounter = 0;
     clearSelected();
+    clearTimer();
 
 }
 function clearSelected(){
@@ -297,6 +356,89 @@ function clearSelected(){
     selectedSameNumber = [];
 }
 
+
+/*------------------------- Popup Management ----------------------------*/
+
+function displayPopup(displayType){
+    switch(displayType){
+        case 'Game Over':
+            popupHeaderEl.textContent = 'Game Over';
+            popupMessageEl.innerHTML = 'You\'ve reached the maximum number of mistakes.<br>Try again with a new puzzle!'
+            popupPrimaryBtnEl.textContent = 'New Game';
+            popupPrimaryBtnEl.dataset.status = 'mistakes';
+            
+            updateClassList(popupstatusEl, 'add', 'block');
+            updateClassList(popupbackdropEl, 'remove', 'hidden');
+        break;
+
+        case 'Pause Timer':
+            updatePopupStatus();
+            popupHeaderEl.textContent = 'Pause Game';
+
+            popupMessageEl.innerHTML = 'Need a moment? Your game is waiting for you.';
+            updateClassList(popupstatusEl, 'remove', 'hidden');
+            popupPrimaryBtnEl.textContent = 'Resume Game';
+            popupPrimaryBtnEl.dataset.status = 'unpause';
+            updateClassList(popupbackdropEl, 'remove', 'hidden');
+            
+
+        break;
+        
+    }
+    
+}
+function updatePopupStatus(){
+    popupDifficultyEl.textContent = currentDifficulty;
+
+    for(let i=1; i<=heartEls.length ; i++){
+        if(i<= mistakesCounter){
+            popupheartsEls[heartEls.length-i].src = './assets/images/Lost-chance.png';
+        }else{
+            popupheartsEls[heartEls.length-i].src = './assets/images/Remaining-chance.png';
+        }
+    }
+    popupTimeEl.textContent = formatTimer(); 
+
+}
+function handlePopupPrimaryBtnClick(){
+ 
+    switch(popupPrimaryBtnEl.dataset.status){
+        case 'mistakes':
+            updateClassList(popupPrimaryBtnEl, 'add', 'tab');
+        
+            setTimeout(() => {
+                updateClassList(popupPrimaryBtnEl, 'remove', 'tab');
+                updateClassList(popupbackdropEl, 'add', 'hidden');
+                startNewGame();
+            }, 50);
+            setTimeout(()=>{
+                updateClassList(popupstatusEl, 'remove', 'block');
+            },300)
+                
+        break;
+        case 'unpause':
+            updateClassList(popupPrimaryBtnEl, 'add', 'tab');
+        
+            setTimeout(() => {
+                updateClassList(popupbackdropEl, 'add', 'hidden');
+                updateClassList(popupPrimaryBtnEl, 'remove', 'tab');
+                updateClassList(popupstatusEl, 'add', 'hidden');
+                resumeTimer();
+            }, 50);
+          
+        break;   
+    }
+
+       
+}
+function handlePopupSecondaryBtnClick(){
+
+}
+
+
+
+
+
  
 /*---------------------------- Validation ----------------------------------*/
 
@@ -308,7 +450,7 @@ function isValidNumber(number){
 
 // Adds, removes, or toggles one or more CSS classes on an element
 function updateClassList(element, action, ...className){
-    element.classList[action](...className);
+    element.classList[action](className);
 }
 
 
@@ -324,24 +466,38 @@ inputPanelEl.addEventListener('click',(event)=>{
 })
 
 document.addEventListener('keydown', (event)=>{
-    
     let key = event.key;
-    let number = Number(key);
-    if(number >= 1 &&  number <= 9){
-        handleNumberSelection(number);
-        const pressedBtn = numberBtnEls.find(btn => Number(btn.textContent) == number);
-        updateClassList(pressedBtn, 'add', 'pressed-number-btn');
-    }
-    else if(key === 'Backspace'){
-        eraseNumber();
-    }else if(key === 'ArrowUp' && selectedRow > 0){
-        setSelectedCell(boardCells[selectedRow - 1][selectedCol]);
-    }else if(key === 'ArrowDown' && selectedRow < 8){
-        setSelectedCell(boardCells[selectedRow + 1][selectedCol]);
-    }else if(key === 'ArrowLeft' && selectedCol > 0){
-        setSelectedCell(boardCells[selectedRow][selectedCol - 1]);
-    }else if(key === 'ArrowRight' && selectedCol < 8){
-        setSelectedCell(boardCells[selectedRow][selectedCol + 1]);
+    // Prevent udpating the board when popup is display
+    if(!popupbackdropEl.classList.contains('hidden')){
+        if(key == ' '){
+            event.preventDefault();
+            handlePopupPrimaryBtnClick();
+        }
+    }else{
+        let number = Number(key);
+        if(number >= 1 &&  number <= 9){
+            handleNumberSelection(number);
+            const pressedBtn = numberBtnEls.find(btn => Number(btn.textContent) == number);
+            updateClassList(pressedBtn, 'add', 'pressed-number-btn');
+        }else if(key === 'Backspace'){
+            eraseNumber();
+        }else if(key === 'ArrowUp' && selectedRow > 0){
+            setSelectedCell(boardCells[selectedRow - 1][selectedCol]);
+        }else if(key === 'ArrowDown' && selectedRow < 8){
+            setSelectedCell(boardCells[selectedRow + 1][selectedCol]);
+        }else if(key === 'ArrowLeft' && selectedCol > 0){
+            setSelectedCell(boardCells[selectedRow][selectedCol - 1]);
+        }else if(key === 'ArrowRight' && selectedCol < 8){
+            setSelectedCell(boardCells[selectedRow][selectedCol + 1]);
+        }else if(key == ' '){
+            event.preventDefault();
+            if(timerBtnEl.dataset.status == 'playing'){
+                pauseTimer();
+            }else{
+                resumeTimer();
+            }
+        }
+
     }
 })
 document.addEventListener('keyup', (event)=>{
@@ -353,4 +509,7 @@ document.addEventListener('keyup', (event)=>{
      }
 })
 
-popupBtnEl.addEventListener('click', handlePopupBtnClick);
+popupPrimaryBtnEl.addEventListener('click', handlePopupPrimaryBtnClick);
+popupSecondaryBtnEl.addEventListener('click', handlePopupSecondaryBtnClick)
+timerBtnEl.addEventListener('click', pauseTimer);
+     
